884ca7854aad40d9b9d63cbd0726fab366b457b4
Alexandre Lecoeur <alexandre.lecoeur@gmail.com>
2023-01-11 16:19:28 +0100

    done

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-let nb, suite = 1;
-nb = prompt("Saisissez un nombre ?")
+let nb=0, suite = 1;
+let affichage = prompt("Saisissez un nombre ?")
 
-for(let i=2 ; i<= nb ; i++) {
-    suite += ` + ${i}`
+for(let i=1 ; i<= nb/2+1 ; i++) {
+    let somme = i;
+    let chaine = " "+nb+ " = "+i+" ";
+    for(let j = i+1; j<= nb/2+1; j++){
+        somme += j;
+        chaine += " + "+j;
+        if(somme == nb){
+            affichage +=`${chaine}`;
+            break;
+        }  else if(somme > nb){
+            break
+        }
+    }
 }
 
-alert(suite);
+alert(affichage);
